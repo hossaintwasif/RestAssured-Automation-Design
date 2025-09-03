47cb3c22bbdb81b5c68c54c8c7de7898b3a8645e
Twasif Hossain <hossaintwasif98@gmail.com>
2025-09-03 22:09:33 +0530

    Initial commit - project setup

---
+++ src/test/java/api/test/UserTests.java
@@ -0,0 +1,80 @@
+package api.test;
+
+import org.testng.Assert;
+import org.testng.AssertJUnit;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import com.github.javafaker.Faker;
+
+import api.endpoints.UserEndpointsMethods;
+import api.payload.User;
+import io.restassured.response.Response;
+
+public class UserTests {
+
+	Faker faker;
+	User userPayload;
+
+	@BeforeMethod
+	public void setupData() {
+		faker = new Faker();
+		userPayload = new User();
+
+		userPayload.setId(faker.idNumber().hashCode());
+		userPayload.setUsername(faker.name().username());
+		userPayload.setFirstname(faker.name().firstName());
+		userPayload.setLastname(faker.name().lastName());
+		userPayload.setEmail(faker.internet().safeEmailAddress());
+		userPayload.setPassword(faker.internet().password(5, 10));
+		userPayload.setPhone(faker.phoneNumber().cellPhone());
+		userPayload.setUserStatus(0);
+	}
+
+	@Test(priority = 1)
+	public void testPostuser() {
+		Response response = UserEndpointsMethods.createUser(userPayload);
+		response.then().log().all();
+
+		AssertJUnit.assertEquals(response.getStatusCode(), 200);
+	}
+	
+	
+	@Test(priority = 2)
+	public void testGetUserByName() {
+		Response response = UserEndpointsMethods.readUser(this.userPayload.getUsername());
+		response.then().log().all();
+		
+		AssertJUnit.assertEquals(response.getStatusCode(), 200);
+	}
+	
+	
+	@Test(priority = 3)
+	public void testUpdateUserByName() {
+		userPayload.setUsername(faker.name().username());
+		userPayload.setFirstname(faker.name().firstName());
+		userPayload.setLastname(faker.name().lastName());
+		
+		Response response = UserEndpointsMethods.updateUser(this.userPayload.getUsername(),userPayload);
+		response.then().log().all();
+		
+		AssertJUnit.assertEquals(response.statusCode(), 200);
+		
+		
+		//Check data after updating
+		Response responseAfter_Update = UserEndpointsMethods.readUser(this.userPayload.getUsername());
+		
+		AssertJUnit.assertEquals(response.getStatusCode(), 200);
+	}
+	
+	
+	@Test(priority = 4)
+	public void testDeleteUserByName() {
+		Response response = UserEndpointsMethods.deleteUser(this.userPayload.getUsername());
+		AssertJUnit.assertEquals(response.getStatusCode(), 200);
+	}
+	
+}
